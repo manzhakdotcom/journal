6cecc50d05974a87f71657006b4ce1942d04d02f
Manzhak <manzhak.com@gmail.com>
2020-11-12 16:42:16 +0300

    add express server

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { LinkContainer as Link } from 'react-router-bootstrap'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {
@@ -6,18 +7,24 @@ const Header = () => {
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>Магазин</Navbar.Brand>
+          <Link to='/'>
+            <Navbar.Brand>Магазин</Navbar.Brand>
+          </Link>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link href='/cart'>
-                <i className='fas fa-shopping-cart'></i>
-                {'  '}Карзина
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <i className='fas fa-user'></i>
-                {'  '}Войти
-              </Nav.Link>
+              <Link to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i>
+                  {'  '}Карзина
+                </Nav.Link>
+              </Link>
+              <Link to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i>
+                  {'  '}Войти
+                </Nav.Link>
+              </Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
